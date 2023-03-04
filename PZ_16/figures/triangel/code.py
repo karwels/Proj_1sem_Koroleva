9b67517dd93b79e07438ae141f311b8cf33cd3a9
karwels <noname08.tss@gmail.com>
2023-03-04 16:20:03 +0300

    PZ_16

---
+++ PZ_16/figures/triangel/code.py
@@ -0,0 +1,9 @@
+a = 7
+b =2
+c = 8
+def triangle_perimeter(a=a,b=b,c=b):
+    P=a+b+c
+    print(P)
+def triangle_area(a=a,b=b,c=b):
+    S=(a*c)/2
+    print(S)
---
+++ PZ_16/test.py
@@ -0,0 +1,4 @@
+from figures import circle_perimeter
+from figures import square_perimeter
+circle_perimeter()
+square_perimeter()
